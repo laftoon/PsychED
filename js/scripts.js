6ac059e5e0eb9e42c722f80908f295ff9f4d6bf0
Laura-Erika Vaida <lauravaida01@gmail.com>
2025-03-05 15:43:39 +0100

    added a top bar nav and stylized the page , added color pallete

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Landing page loaded!");
 
-    const button = document.querySelector(".btn-hover");
-    button.addEventListener("click", function() {
-        alert("You clicked the button!");
-    });
 });
