01b3632546097ded393ef7d44505ea2df0bfb260
Laura-Erika Vaida <lauravaida01@gmail.com>
2025-03-10 19:56:24 +0100

    implemented the email message in the contact form

--- PyschED/PyschED/settings.py
+++ PyschED/PyschED/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os 
+# Messages framework
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +58,13 @@ SECRET_KEY = 'django-insecure-3f8s(cn1)%*2k%fpl+73v7)ov3kiwjns2ybu*9mo8$y1zubrzs
 DEBUG = True
 
 ALLOWED_HOSTS = []
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'lauravaida01@gmail.com'  # Your Gmail address
+EMAIL_HOST_PASSWORD = 'dzip absb bvms ebwf'  # Your Gmail app password
 
 # Application definition
 INSTALLED_APPS = [
