0b49f64f1f06ce59ed6308aab37495a2451b7c43
Laura-Erika Vaida <lauravaida01@gmail.com>
2025-03-11 16:55:39 +0100

    remade the contact form to be bigger and have more options

--- PyschED/core/forms.py
+++ PyschED/core/forms.py
@@ -4,6 +4,8 @@ class ContactForm(forms.Form):
     firstName = forms.CharField(max_length=100)
     lastName = forms.CharField(max_length=100)
     email = forms.EmailField()
+    interest = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
 
     def clean_email(self):
         email = self.cleaned_data['email']
